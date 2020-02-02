fb1993c279c16c86803c871a76bbb99f8d42f76f
rjsears <richardjsears@gmail.com>
2020-02-02 00:53:47 -0700

    Update to version 0.0.2

--- update_fish_tank_data.py
+++ update_fish_tank_data.py
@@ -1,69 +1,104 @@
 #!/usr/bin/python3
-  
-## Reads fish tank data utilizing Seneye API and writes data to InfluxDB
-## January 31, 2020
 
-## In order to use this script you need to have the Seneye Reef Monitoring system and have setup API access
-## to it. You can then use something like GRafana to graph the results. I run this once every ten minutes
-## from a cron entry.
+__author__ = 'Richard J. Sears'
+VERSION = "V0.0.2 (2020-02-02)"
+# richardjsears@gmail.com
 
-# */10 * * * * /root/fish_tank_monitoring/update_fish_tank_data.py >/dev/null 2>&1
+# Reads fish tank data utilizing Seneye API and writes data to InfluxDB
+# February 2nd, 2020
 
-## Also make sure you have the influxdb client installed: pip install influxdb
+# More information about the Seneye API may be found here:
+# http://answers.seneye.com/en/Seneye_Products/Seneye_hobbyist_developer_information
+# and here:
+# https://api.seneye.com/
 
-## You will need to setup API access on the Seneye website and get your device_id and creds to use the API. 
-## Be sure to replace the word "device_id" in the https request below with your actual device_id as well as
-## your email address and password otherwise this will fail.
-
-from influxdb import InfluxDBClient
+# pip3 install influxdb if you have not already done so.
+from influxdb import InfluxDBClient, exceptions
 import requests
 import json
+
+# Set DEBUG = True (or 1) to output data to the screen, otherwise False (or 0) to
+# record data to the database. When DEBUG = True, NO DATA will be written to the
+# database.
+DEBUG = False
 
 # InfluxDB connections settings
 influx_host = 'my_influxdb_host'
 influx_port = 8086
 influx_user = 'aquaman'
-influx_password = 'breathewater!'
+influx_password = 'breathewater'
 influx_dbname = 'fish_tank'
 
-def write_data(measurement, value):
-    client = InfluxDBClient(influx_host, influx_port, influx_user, influx_password, influx_dbname)
-    json_body = [
-            {
-                "measurement": measurement,
-                "tags": {
-                    "tank": "75planted"
-                        },
-                "fields": {
-                    "value" : value
-                }
-            }
-        ]
+# This is useful if you have multiple tanks you want to keep track of...
+tank_name = "75planted"
 
-    client.write_points(json_body)
+# Seneye API Settings
+user_email = "your_email_address@gmail.com"
+user_passwd = "super_secret_password"
+user_device = "12345"
+
+# If you are like me and want to see your tank water temperature in Fahrenheit, set to True
+Temp_in_F = True
+
+
+def write_data(measurement, value):
+    client = InfluxDBClient(influx_host, influx_port, influx_user, influx_password, influx_dbname, timeout=2)
+    json_body = [
+        {
+            "measurement": measurement,
+            "tags": {
+                "tank": tank_name
+            },
+            "fields": {
+                "value": value
+            }
+        }
+    ]
+    try:
+        client.write_points(json_body)
+        client.close()
+    except (exceptions.InfluxDBClientError, exceptions.InfluxDBServerError) as e:
+        print(e)
+
 
 def get_fish_tank_data():
-    api_request = requests.get(
-        'https://api.seneye.com/v1/devices/device_id?IncludeState=1&user=your_email_address@somewhere.com&pwd=letmein',
-        headers={'Accept': 'application/json', 'Content-Type': 'application/json'},
-    )
+    url = "https://api.seneye.com/v1/devices/{}?IncludeState=1&user={}&pwd={}".format(user_device, user_email, user_passwd)
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    api_request = requests.get(url, headers=headers)
     response = api_request.json()
     to_json = json.dumps(response)
     dict_json = json.loads(to_json)
 
-    keys = ['ph', 'nh3', 'nh4', 'o2', 'lux', 'par', 'kelvin']
-    for key in dict_json['exps']:
-        if key in keys:
-            value = dict_json['exps'][key]['curr']
-            write_data(key, float(value))
-    # Convert temperature from C to F
-    temp_in_c = float(dict_json['exps']['temperature']['curr'])
-    temp_in_f = (9.0/5.0 * temp_in_c) + 32
-    write_data('temperature', temp_in_f)
+    if Temp_in_F:
+        keys = ['ph', 'nh3', 'nh4', 'o2', 'lux', 'par', 'kelvin']
+        for key in dict_json['exps']:
+            if key in keys:
+                value = dict_json['exps'][key]['curr']
+                if DEBUG:
+                    print(key, value)
+                else:
+                    write_data(key, float(value))
+        # Convert temperature from C to F
+        temp_in_c = float(dict_json['exps']['temperature']['curr'])
+        temp_in_f = (9.0 / 5.0 * temp_in_c) + 32
+        if DEBUG:
+            print("temperature", temp_in_f)
+        else:
+            write_data('temperature', temp_in_f)
+    else:
+        keys = ['temperature', 'ph', 'nh3', 'nh4', 'o2', 'lux', 'par', 'kelvin']
+        for key in dict_json['exps']:
+            if key in keys:
+                value = dict_json['exps'][key]['curr']
+                if DEBUG:
+                    print(key, value)
+                else:
+                    write_data(key, float(value))
 
 
 def main():
-        get_fish_tank_data()
+    get_fish_tank_data()
+
 
 if __name__ == '__main__':
     main()
